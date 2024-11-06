7198eb62e5e8a26268499b14cca132f88afc2e8c
gominzip <ahaminji@naver.com>
2024-11-06 23:47:35 +0900

    feat: 섹션 구분 더보기 버튼 구현 (#71)

---
+++ frontend/src/components/main/LoadMoreDivider.tsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+import ChevronDownIcon from '@assets/icons/chevron-down.svg';
+
+interface LoadMoreDividerProps {
+  text: string;
+  onClick?: () => void;
+}
+
+const MoreButton = ({ text, onClick }: LoadMoreDividerProps) => {
+  return (
+    <MoreButtonBox onClick={onClick}>
+      <span>{text}</span>
+      <StyledChevronDown />
+    </MoreButtonBox>
+  );
+};
+
+const LoadMoreDivider = ({ text, onClick = () => {} }: LoadMoreDividerProps) => {
+  return (
+    <LoadMoreDividerBox>
+      <MoreButton text={text} onClick={onClick} />
+    </LoadMoreDividerBox>
+  );
+};
+
+export default LoadMoreDivider;
+
+const MoreButtonBox = styled.button`
+  width: 87px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.tokenColor['text-default']};
+  ${({ theme }) => theme.typographyStyle['display-medium12']}
+  background-color: ${({ theme }) => theme.tokenColor['surface-default']};
+  border: 1px solid ${({ theme }) => theme.tokenColor['surface-alt']};
+  border-radius: 15px;
+  margin: 0 16px;
+  cursor: pointer;
+  span {
+    padding-left: 5px;
+  }
+`;
+
+const StyledChevronDown = styled(ChevronDownIcon)`
+  width: 16px;
+  height: 16px;
+`;
+
+const LoadMoreDividerBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:before,
+  &:after {
+    content: '';
+    flex: 1;
+    height: 1px;
+    background-color: ${({ theme }) => theme.tokenColor['text-strong']};
+  }
+`;
