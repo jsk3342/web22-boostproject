e91ca7e5654a35f3c96bdfc41c65159cd41953a1
jisu-kim <kss2460@naver.com>
2024-11-13 17:38:17 +0900

    chore: api 구조 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,26 @@ import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles/theme';
 import { MainPage, ClientPage, HostPage } from './pages';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@apis/index';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
 
-          <Route path="/live" element={<ClientPage />} />
-          <Route path="/live/:id" element={<ClientPage />} />
+            <Route path="/live" element={<ClientPage />} />
+            <Route path="/live/:id" element={<ClientPage />} />
 
-          <Route path="/host" element={<HostPage />} />
-          <Route path="/host/:id" element={<HostPage />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+            <Route path="/host" element={<HostPage />} />
+            <Route path="/host/:id" element={<HostPage />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
