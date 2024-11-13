ddbef30073109aa872d2771bfa539035837477a8
hoeeeeeh <hoeeeeeh@naver.com>
2024-11-13 15:33:52 +0900

    refactor: vite mode 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    hmr: mode !== 'production',  // 프로덕션 환경에서만 HMR 비활성화
+  },
   plugins: [
     react(),
     tsconfigPaths(),
@@ -16,4 +20,4 @@ export default defineConfig({
     outDir: '.dist'
   },
   cacheDir: '.vite'
-});
+}))
