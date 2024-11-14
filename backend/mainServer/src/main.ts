a1095b46bc65f3c95a546f8b037555ce74ad8c95
i3kae <snoger1234@gmail.com>
2024-11-14 14:20:04 +0900

    feat: 기존 main server에 swagger setup 추가

--- backend/mainServer/src/main.ts
+++ backend/mainServer/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { setupSwagger } from './util/swagger.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
