68464153033051710ace02138b4b8289885168c3
jisu-kim <kss2460@naver.com>
2024-11-14 16:27:31 +0900

    feat: queries에 들어갈 fetch 훅 구현

--- frontend/src/apis/host/fetchStreamKey.ts
+++ frontend/src/apis/host/fetchStreamKey.ts
@@ -1,9 +1,14 @@
+import { AxiosResponse } from 'axios';
 import { BASE_URL, fetchInstance } from '..';
+
+type StreamKeyResponse = {
+  'stream-key': string;
+  'session-key': string;
+};
 
 type NanoId = string;
 
-export const fetchStreamKey = (userId: NanoId) => {
-  return fetchInstance().post(`${BASE_URL}/host/key`, {
-    userId
-  });
+export const fetchStreamKey = async (userId: NanoId): Promise<StreamKeyResponse> => {
+  const response: AxiosResponse<StreamKeyResponse> = await fetchInstance().post(`${BASE_URL}/host/key`, { userId });
+  return response.data;
 };
