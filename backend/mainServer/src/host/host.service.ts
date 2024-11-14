04e6808b5b8fa5549b905a6a1a88333ff4e4255a
i3kae <snoger1234@gmail.com>
2024-11-14 14:52:02 +0900

    feat: host 스트림키 생성 api swagger 작성 / swagger 요청에 맞춰 body 데이터 DTO로 작성

--- backend/mainServer/src/host/host.service.ts
+++ backend/mainServer/src/host/host.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@nestjs/common';
 import dotenv from 'dotenv';
 import path from 'path';
 import crypto from 'crypto';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class keyGenerateRequestDto {
+  @ApiProperty({example: 'this_is_uuid', description: '클라이언트마다 가지는 랜덤한 uuid 값'})
+    uuid: string = '';
+}
 
 function generateRandomKey(uuid: string, salt: string) {
   const hash = crypto.createHmac('sha256', salt).update(uuid).digest('hex');
@@ -10,10 +16,10 @@ function generateRandomKey(uuid: string, salt: string) {
 
 @Injectable()
 export class HostService {
-  async generateStreamKey(uuid: string): Promise<Array<string>> {
+  async generateStreamKey(requestDto: keyGenerateRequestDto): Promise<Array<string>> {
     dotenv.config({path: path.resolve('../.env')});
-    const streamKey = generateRandomKey(uuid, process.env.STREAM_KEY_SALT || '');
-    const sessionKey = generateRandomKey(uuid, process.env.SESSION_KEY_SALT || '');
+    const streamKey = generateRandomKey(requestDto.uuid, process.env.STREAM_KEY_SALT || '');
+    const sessionKey = generateRandomKey(requestDto.uuid, process.env.SESSION_KEY_SALT || '');
     return [streamKey, sessionKey];
   }
 }
