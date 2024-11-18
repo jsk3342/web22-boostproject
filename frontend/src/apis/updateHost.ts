0cb7d8b0dc7ca977011681c23a0501e986f4205e
jisu-kim <kss2460@naver.com>
2024-11-18 22:00:56 +0900

    feat: 호스트 업데이트 api 구현

---
+++ frontend/src/apis/updateHost.ts
@@ -0,0 +1,16 @@
+import { AxiosResponse } from 'axios';
+import { fetchInstance } from '.';
+
+export interface HostInfo {
+  userId: string;
+  liveTitle: string;
+  defaultThumbnailImageUrl: '';
+  category: string;
+  tags: string[];
+}
+
+export const updateHost = async (hostInfo: HostInfo): Promise<HostInfo> => {
+  const response: AxiosResponse<HostInfo> = await fetchInstance().post('/host/update', hostInfo);
+
+  return response.data;
+};
