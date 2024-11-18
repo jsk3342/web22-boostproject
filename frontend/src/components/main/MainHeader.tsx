49d5bd0bece1ae2baf92e50e3f13384da07a20f9
Changhyun-Hong <alex963@naver.com>
2024-11-18 21:10:52 +0900

    feat: mainPage header 구현

--- frontend/src/components/main/MainHeader.tsx
+++ frontend/src/components/main/MainHeader.tsx
@@ -1,19 +1,121 @@
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import Logo from '@assets/logo.gif';
+import SearchIcon from '@assets/icons/search_icon.svg';
+import VideoIcon from '@assets/icons/video_icon.svg';
 
 const MainHeader = () => {
-  return <HeaderContainer>MainHeader</HeaderContainer>;
+  const navigate = useNavigate();
+
+  return (
+    <HeaderContainer>
+      <LogoContainer onClick={() => window.location.reload()}>
+        <img src={Logo} alt="로고" />
+      </LogoContainer>
+      <SearchBox>
+        <SearchInputWrapper>
+          <SearchInput type="text" placeholder="컨퍼런스, 태그 검색" />
+          <SearchIconStyled />
+        </SearchInputWrapper>
+        <SearchButton />
+      </SearchBox>
+      <StudioBox onClick={() => navigate('/host')}>
+        <VideoIconStyled />
+        스튜디오
+      </StudioBox>
+    </HeaderContainer>
+  );
 };
 
 export default MainHeader;
 
 const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
   height: 60px;
   min-width: 800px;
   position: fixed;
-  left: 0;
+  left: 45px;
   top: 0;
-  right: 0;
+  right: 45px;
   z-index: 11000;
   background: ${({ theme }) => theme.tokenColors['surface-default']};
   color: ${({ theme }) => theme.tokenColors['text-strong']};
 `;
+
+const LogoContainer = styled.div`
+  height: 30px;
+  cursor: pointer;
+  
+  img {
+    height: 100%;
+  }
+`;
+
+const SearchBox = styled.div`
+  display: flex;
+  align-items: center;
+  width: 400px;
+`;
+
+const SearchInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 10px 40px 10px 20px;
+  border: 1px solid ${({ theme }) => theme.colorMap.gray[900]};
+  border-radius: 20px;
+  color: ${({ theme }) => theme.tokenColors['text-strong']};
+  background-color: ${({ theme }) => theme.tokenColors['surface-default']};
+
+  &:focus {
+    border-color: ${({ theme }) => theme.tokenColors['brand-default']};
+    outline: none;
+  }
+`;
+
+const SearchIconStyled = styled(SearchIcon)`
+  position: absolute;
+  top: 50%;
+  right: -50px;
+  transform: translateY(-50%);
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`;
+
+const SearchButton = styled.button`
+  background: ${({ theme }) => theme.tokenColors['primary-default']};
+  color: ${({ theme }) => theme.tokenColors['color-white']};
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-left: 10px;
+`;
+
+const StudioBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 10px 0 0;
+  ${({ theme }) => theme.tokenTypographys['display-bold16']};
+  background-color: ${({ theme }) => theme.tokenColors['primary-default']};
+  color: ${({ theme }) => theme.tokenColors['color-white']};
+  border: 1px solid ${({ theme }) => theme.colorMap.gray[900]};
+  border-radius: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colorMap.gray[900]};
+  }
+`;
+
+const VideoIconStyled = styled(VideoIcon)``;
