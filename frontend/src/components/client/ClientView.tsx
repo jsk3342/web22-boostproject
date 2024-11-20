78eee395dd1124d5f052ff81fb4547c2682d3318
Changhyun-Hong <alex963@naver.com>
2024-11-21 00:07:51 +0900

    feat: 클라이언트 페이지에서 영상 보여주기

--- frontend/src/components/client/ClientView.tsx
+++ frontend/src/components/client/ClientView.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
 
 import Player from './Player';
 import PlayerInfo from './PlayerInfo';
 import Footer from './Footer';
 
 const ClientView = () => {
+  const { id: liveId } = useParams();
+
   return (
     <ClientViewContainer>
       <h1 className="hidden">클라이언트 페이지</h1>
-      <Player videoUrl={'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'} />
+      <Player videoUrl={`https://kr.object.ncloudstorage.com/web22/live/${liveId}/index.m3u8`} />
       <PlayerInfo />
       <Footer />
     </ClientViewContainer>
