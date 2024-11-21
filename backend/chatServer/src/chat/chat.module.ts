76464b146367badcc25e3eb76bfdc50571e31148
hoeeeeeh <hoeeeeeh@naver.com>
2024-11-22 00:09:37 +0900

    refactor: room 마다 유저 Id 하나씩 강제하도록 변경

--- backend/chatServer/src/chat/chat.module.ts
+++ backend/chatServer/src/chat/chat.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway, checkHostUser, checkValidUser } from './chat.gateway';
-import { UserModule } from '../user/user.module';
 import { RoomService } from '../room/room.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [],
   providers: [ChatGateway, checkValidUser, checkHostUser, RoomService],
 })
 export class ChatModule {}
