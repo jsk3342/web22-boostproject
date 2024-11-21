e776cd4ebdf5f5e8f500d64ce469d9a3d5795fe9
hoeeeeeh <hoeeeeeh@naver.com>
2024-11-21 16:05:11 +0900

    feat: dockerfile, docker-compose

--- backend/chatServer/src/main.ts
+++ backend/chatServer/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // CORS 설정 (웹 클라이언트와의 연결을 허용)
-  await app.listen(3000);
-  console.log('Chat server is running on http://localhost:3000');
+  await app.listen(4000);
+  console.log('Chat server is running on http://localhost:4000');
 }
 bootstrap();
