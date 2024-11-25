5f3fb33b5ff903e7e29c8929f2f5fdf70226d4e5
i3kae <snoger1234@gmail.com>
2024-11-25 17:28:10 +0900

    feat: 단일 replay 영상에 대한 정보 반환 API

--- backend/mainServer/src/replay/replay.controller.ts
+++ backend/mainServer/src/replay/replay.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Query, HttpException } from '@nestjs/common';
 import { Response } from 'express';
 import { ReplayService } from './replay.service.js';
 import { MemoryDBService } from '../memory-db/memory-db.service.js';
@@ -10,7 +10,7 @@ export class ReplayController {
   constructor(private readonly replayService: ReplayService, private readonly memoryDBService: MemoryDBService) {}
 
   @Get('/latest')
-  async getLatestSession(@Res() res: Response) {
+  async getLatestReplay(@Res() res: Response) {
     try {
       const replayChecker = (item: MemoryDbDto) => item.replay;
       const serchedData = this.memoryDBService.getBroadcastInfo(8, memoryDbDtoToReplayVideoDto, replayChecker);
@@ -28,4 +28,27 @@ export class ReplayController {
       }
     }
   }
+
+  @Get('/video')
+  async getReplayInfo(@Query('videoId') sessionKey: string, @Res() res: Response) {
+    try {
+      const sessionInfo = this.memoryDBService.findBySessionKey(sessionKey);
+      if (!sessionInfo && !sessionInfo.replay) {
+        throw new HttpException('No Available Session', HttpStatus.BAD_REQUEST);
+      }
+      res.status(HttpStatus.OK).json({info : memoryDbDtoToReplayVideoDto(sessionInfo)});
+    } catch (error) {
+      if ((error as { status: number }).status === 400) {
+        res.status(HttpStatus.BAD_REQUEST).json({
+          error: (error as { response: Response }).response
+        });
+      }
+      else {
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          error: 'Server logic error',
+        });
+      }
+    }
+  }
+
 }
