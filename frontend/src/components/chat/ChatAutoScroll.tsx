29236550068ee8e86d4857b641f812d3370f78ef
gominzip <ahaminji@naver.com>
2024-11-27 15:32:24 +0900

    feat: 오토 스크롤 버튼 유저 역할에 따른 아이콘 추가

--- frontend/src/components/chat/ChatAutoScroll.tsx
+++ frontend/src/components/chat/ChatAutoScroll.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import ChevronDownIcon from '@assets/icons/chevron-down.svg';
 import { MessageReceiveData } from '@type/chat';
+import HostIconGreen from '@assets/icons/host_icon_green.svg';
 
 interface ChatAutoScrollProps {
   currentChat?: MessageReceiveData | null;
@@ -12,9 +13,17 @@ const ChatAutoScroll = ({ currentChat, isAtBottom, scrollToBottom }: ChatAutoScr
   return !isAtBottom ? (
     <ScrollToBottomButton $hasMsg={!!currentChat} onClick={scrollToBottom}>
       {currentChat && (
-        <NormalChat $pointColor={currentChat.color}>
+        <NormalChat
+          $isHost={currentChat.owner === 'host'}
+          $pointColor={currentChat.owner === 'host' ? '#0ADD91' : currentChat.color}
+        >
+          {currentChat.owner === 'me' ? (
+            <span className="text_point">🧀</span>
+          ) : currentChat.owner === 'host' ? (
+            <StyledIcon as={HostIconGreen} />
+          ) : null}
           <span className="text_point">{currentChat.nickname}</span>
-          <span className="text_msg">{currentChat.msg}</span>
+          <span className="chat_message">{currentChat.msg}</span>
         </NormalChat>
       )}
       <StyledChevronDown />
@@ -51,15 +60,16 @@ const StyledChevronDown = styled(ChevronDownIcon)`
   height: 28px;
 `;
 
-const NormalChat = styled.div<{ $pointColor: string }>`
+const NormalChat = styled.div<{ $isHost: boolean; $pointColor: string }>`
   ${({ theme }) => theme.tokenTypographys['display-medium14']};
-  color: ${({ theme }) => theme.tokenColors['color-white']};
+  color: ${({ $isHost, theme }) => ($isHost ? theme.tokenColors['color-accent'] : theme.tokenColors['color-white'])};
   display: flex;
   align-items: center;
   justify-content: flex-start;
   width: 90%;
 
-  .text_msg {
+  .chat_message {
+    color: ${({ $isHost }) => $isHost && '#82e3c4'};
     flex: 1 1;
     overflow: hidden;
     text-align: left;
@@ -69,5 +79,13 @@ const NormalChat = styled.div<{ $pointColor: string }>`
   .text_point {
     color: ${({ $pointColor }) => $pointColor};
     margin-right: 5px;
+    ${({ theme }) => theme.tokenTypographys['display-bold14']};
   }
+`;
+
+const StyledIcon = styled.svg`
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  margin: 0 5px -3px 0;
 `;
